0581487bb530079ca72a058077ecbcb296afedf4
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-10-11 12:26:17 -0500

    bloo

--- src/widgets/collections/simple-visual.ts
+++ src/widgets/collections/simple-visual.ts
@@ -2,16 +2,14 @@ import type { WidgetCollection } from "@/widgets/collections/types"
 import { allWidgetDefinitions } from "@/widgets/definitions"
 
 export const simpleVisualCollection: WidgetCollection<
-	Pick<typeof allWidgetDefinitions, "emojiImage" | "urlImage" | "vennDiagram" | "periodicTable" | "video">,
-	readonly ["emojiImage", "urlImage", "vennDiagram", "periodicTable", "video"]
+	Pick<typeof allWidgetDefinitions, "emojiImage" | "urlImage" | "video">,
+	readonly ["emojiImage", "urlImage", "video"]
 > = {
 	name: "simple-visual",
 	widgets: {
 		emojiImage: allWidgetDefinitions.emojiImage,
 		urlImage: allWidgetDefinitions.urlImage,
-		vennDiagram: allWidgetDefinitions.vennDiagram,
-		periodicTable: allWidgetDefinitions.periodicTable,
 		video: allWidgetDefinitions.video
 	},
-	widgetTypeKeys: ["emojiImage", "urlImage", "vennDiagram", "periodicTable", "video"]
+	widgetTypeKeys: ["emojiImage", "urlImage", "video"]
 } as const
