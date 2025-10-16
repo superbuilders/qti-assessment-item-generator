863907612507a6a5246abd4e595cbaf73832baa1
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-10-16 13:29:51 -0500

    bloo

--- src/widgets/collections/math-core.ts
+++ src/widgets/collections/math-core.ts
@@ -1,113 +1,171 @@
-import {
-	AngleDiagramPropsSchema,
-	BarChartPropsSchema,
-	BoxGridPropsSchema,
-	BoxPlotPropsSchema,
-	CircleDiagramPropsSchema,
-	CompositeShapeDiagramPropsSchema,
-	CoordinatePlaneComprehensivePropsSchema,
-	DiscreteObjectRatioDiagramPropsSchema,
-	DotPlotPropsSchema,
-	DoubleNumberLinePropsSchema,
-	EmojiImagePropsSchema,
-	FigureComparisonDiagramPropsSchema,
-	FunctionPlotGraphPropsSchema,
-	GeometricSolidDiagramPropsSchema,
-	HangerDiagramPropsSchema,
-	HistogramPropsSchema,
-	InequalityNumberLinePropsSchema,
-	IntegerChipLegendPropsSchema,
-	IntegerChipModelPropsSchema,
-	LineEquationGraphPropsSchema,
-	MarbleDiagramPropsSchema,
-	NPolygonPropsSchema,
-	NumberLinePropsSchema,
-	NumberLineWithActionPropsSchema,
-	NumberLineWithFractionGroupsPropsSchema,
-	NumberSetDiagramPropsSchema,
-	ParallelogramTrapezoidDiagramPropsSchema,
-	PartitionedShapePropsSchema,
-	PentagonIntersectionDiagramPropsSchema,
-	PictographPropsSchema,
-	PointPlotGraphPropsSchema,
-	PolygonGraphPropsSchema,
-	PolyhedronDiagramPropsSchema,
-	PolyhedronNetDiagramPropsSchema,
-	ProbabilitySpinnerPropsSchema,
-	PythagoreanProofDiagramPropsSchema,
-	QuadrantDiagramPropsSchema,
-	RadiallyConstrainedAngleDiagramPropsSchema,
-	RatioBoxDiagramPropsSchema,
-	RectangularFrameDiagramPropsSchema,
-	ScaleCopiesSliderPropsSchema,
-	ScatterPlotPropsSchema,
-	ShapeTransformationGraphPropsSchema,
-	StackedItemsDiagramPropsSchema,
-	TapeDiagramPropsSchema,
-	ThreeDIntersectionDiagramPropsSchema,
-	TransformationDiagramPropsSchema,
-	TreeDiagramPropsSchema,
-	TriangleDiagramPropsSchema,
-	UnitBlockDiagramPropsSchema,
-	VennDiagramPropsSchema,
-	VerticalArithmeticSetupPropsSchema
-} from "../registry"
+import type { WidgetCollection } from "@/widgets/collections/types"
+import { allWidgetDefinitions } from "@/widgets/definitions"
 
-export const mathCoreCollection = {
+export const mathCoreCollection: WidgetCollection<
+	Pick<
+		typeof allWidgetDefinitions,
+		| "threeDIntersectionDiagram"
+		| "quadrantDiagram"
+		| "angleDiagram"
+		| "radiallyConstrainedAngleDiagram"
+		| "barChart"
+		| "boxGrid"
+		| "boxPlot"
+		| "circleDiagram"
+		| "compositeShapeDiagram"
+		| "coordinatePlane"
+		| "discreteObjectRatioDiagram"
+		| "dotPlot"
+		| "doubleNumberLine"
+		| "emojiImage"
+		| "figureComparisonDiagram"
+		| "functionPlotGraph"
+		| "geometricSolidDiagram"
+		| "hangerDiagram"
+		| "histogram"
+		| "inequalityNumberLine"
+		| "marbleDiagram"
+		| "lineEquationGraph"
+		| "numberLine"
+		| "numberLineWithAction"
+		| "numberLineWithFractionGroups"
+		| "numberSetDiagram"
+		| "parallelogramTrapezoidDiagram"
+		| "partitionedShape"
+		| "pentagonIntersectionDiagram"
+		| "pictograph"
+		| "integerChipModel"
+		| "integerChipLegend"
+		| "pointPlotGraph"
+		| "polygonGraph"
+		| "polyhedronDiagram"
+		| "polyhedronNetDiagram"
+		| "probabilitySpinner"
+		| "pythagoreanProofDiagram"
+		| "ratioBoxDiagram"
+		| "rectangularFrameDiagram"
+		| "scaleCopiesSlider"
+		| "scatterPlot"
+		| "shapeTransformationGraph"
+		| "stackedItemsDiagram"
+		| "tapeDiagram"
+		| "transformationDiagram"
+		| "treeDiagram"
+		| "triangleDiagram"
+		| "unitBlockDiagram"
+		| "vennDiagram"
+		| "verticalArithmeticSetup"
+		| "nPolygon"
+	>,
+	readonly [
+		"threeDIntersectionDiagram",
+		"quadrantDiagram",
+		"angleDiagram",
+		"radiallyConstrainedAngleDiagram",
+		"barChart",
+		"boxGrid",
+		"boxPlot",
+		"circleDiagram",
+		"compositeShapeDiagram",
+		"coordinatePlane",
+		"discreteObjectRatioDiagram",
+		"dotPlot",
+		"doubleNumberLine",
+		"emojiImage",
+		"figureComparisonDiagram",
+		"functionPlotGraph",
+		"geometricSolidDiagram",
+		"hangerDiagram",
+		"histogram",
+		"inequalityNumberLine",
+		"marbleDiagram",
+		"lineEquationGraph",
+		"numberLine",
+		"numberLineWithAction",
+		"numberLineWithFractionGroups",
+		"numberSetDiagram",
+		"parallelogramTrapezoidDiagram",
+		"partitionedShape",
+		"pentagonIntersectionDiagram",
+		"pictograph",
+		"integerChipModel",
+		"integerChipLegend",
+		"pointPlotGraph",
+		"polygonGraph",
+		"polyhedronDiagram",
+		"polyhedronNetDiagram",
+		"probabilitySpinner",
+		"pythagoreanProofDiagram",
+		"ratioBoxDiagram",
+		"rectangularFrameDiagram",
+		"scaleCopiesSlider",
+		"scatterPlot",
+		"shapeTransformationGraph",
+		"stackedItemsDiagram",
+		"tapeDiagram",
+		"transformationDiagram",
+		"treeDiagram",
+		"triangleDiagram",
+		"unitBlockDiagram",
+		"vennDiagram",
+		"verticalArithmeticSetup",
+		"nPolygon"
+	]
+> = {
 	name: "math-core",
-	schemas: {
-		quadrantDiagram: QuadrantDiagramPropsSchema,
-		threeDIntersectionDiagram: ThreeDIntersectionDiagramPropsSchema,
-		angleDiagram: AngleDiagramPropsSchema,
-		radiallyConstrainedAngleDiagram: RadiallyConstrainedAngleDiagramPropsSchema,
-		barChart: BarChartPropsSchema,
-		boxGrid: BoxGridPropsSchema,
-		boxPlot: BoxPlotPropsSchema,
-		circleDiagram: CircleDiagramPropsSchema,
-		compositeShapeDiagram: CompositeShapeDiagramPropsSchema,
-		coordinatePlane: CoordinatePlaneComprehensivePropsSchema,
-		discreteObjectRatioDiagram: DiscreteObjectRatioDiagramPropsSchema,
-		dotPlot: DotPlotPropsSchema,
-		doubleNumberLine: DoubleNumberLinePropsSchema,
-		emojiImage: EmojiImagePropsSchema,
-		figureComparisonDiagram: FigureComparisonDiagramPropsSchema,
-		functionPlotGraph: FunctionPlotGraphPropsSchema,
-		geometricSolidDiagram: GeometricSolidDiagramPropsSchema,
-		hangerDiagram: HangerDiagramPropsSchema,
-		histogram: HistogramPropsSchema,
-		inequalityNumberLine: InequalityNumberLinePropsSchema,
-		marbleDiagram: MarbleDiagramPropsSchema,
-		lineEquationGraph: LineEquationGraphPropsSchema,
-		numberLine: NumberLinePropsSchema,
-		numberLineWithAction: NumberLineWithActionPropsSchema,
-		numberLineWithFractionGroups: NumberLineWithFractionGroupsPropsSchema,
-		numberSetDiagram: NumberSetDiagramPropsSchema,
-		parallelogramTrapezoidDiagram: ParallelogramTrapezoidDiagramPropsSchema,
-		partitionedShape: PartitionedShapePropsSchema,
-		pentagonIntersectionDiagram: PentagonIntersectionDiagramPropsSchema,
-		pictograph: PictographPropsSchema,
-		integerChipModel: IntegerChipModelPropsSchema,
-		integerChipLegend: IntegerChipLegendPropsSchema,
-		pointPlotGraph: PointPlotGraphPropsSchema,
-		polygonGraph: PolygonGraphPropsSchema,
-		polyhedronDiagram: PolyhedronDiagramPropsSchema,
-		polyhedronNetDiagram: PolyhedronNetDiagramPropsSchema,
-		probabilitySpinner: ProbabilitySpinnerPropsSchema,
-		pythagoreanProofDiagram: PythagoreanProofDiagramPropsSchema,
-		ratioBoxDiagram: RatioBoxDiagramPropsSchema,
-		rectangularFrameDiagram: RectangularFrameDiagramPropsSchema,
-		scaleCopiesSlider: ScaleCopiesSliderPropsSchema,
-		scatterPlot: ScatterPlotPropsSchema,
-		shapeTransformationGraph: ShapeTransformationGraphPropsSchema,
-		stackedItemsDiagram: StackedItemsDiagramPropsSchema,
-		tapeDiagram: TapeDiagramPropsSchema,
-		transformationDiagram: TransformationDiagramPropsSchema,
-		treeDiagram: TreeDiagramPropsSchema,
-		triangleDiagram: TriangleDiagramPropsSchema,
-		unitBlockDiagram: UnitBlockDiagramPropsSchema,
-		vennDiagram: VennDiagramPropsSchema,
-		verticalArithmeticSetup: VerticalArithmeticSetupPropsSchema,
-		nPolygon: NPolygonPropsSchema
+	widgets: {
+		threeDIntersectionDiagram: allWidgetDefinitions.threeDIntersectionDiagram,
+		quadrantDiagram: allWidgetDefinitions.quadrantDiagram,
+		angleDiagram: allWidgetDefinitions.angleDiagram,
+		radiallyConstrainedAngleDiagram: allWidgetDefinitions.radiallyConstrainedAngleDiagram,
+		barChart: allWidgetDefinitions.barChart,
+		boxGrid: allWidgetDefinitions.boxGrid,
+		boxPlot: allWidgetDefinitions.boxPlot,
+		circleDiagram: allWidgetDefinitions.circleDiagram,
+		compositeShapeDiagram: allWidgetDefinitions.compositeShapeDiagram,
+		coordinatePlane: allWidgetDefinitions.coordinatePlane,
+		discreteObjectRatioDiagram: allWidgetDefinitions.discreteObjectRatioDiagram,
+		dotPlot: allWidgetDefinitions.dotPlot,
+		doubleNumberLine: allWidgetDefinitions.doubleNumberLine,
+		emojiImage: allWidgetDefinitions.emojiImage,
+		figureComparisonDiagram: allWidgetDefinitions.figureComparisonDiagram,
+		functionPlotGraph: allWidgetDefinitions.functionPlotGraph,
+		geometricSolidDiagram: allWidgetDefinitions.geometricSolidDiagram,
+		hangerDiagram: allWidgetDefinitions.hangerDiagram,
+		histogram: allWidgetDefinitions.histogram,
+		inequalityNumberLine: allWidgetDefinitions.inequalityNumberLine,
+		marbleDiagram: allWidgetDefinitions.marbleDiagram,
+		lineEquationGraph: allWidgetDefinitions.lineEquationGraph,
+		numberLine: allWidgetDefinitions.numberLine,
+		numberLineWithAction: allWidgetDefinitions.numberLineWithAction,
+		numberLineWithFractionGroups: allWidgetDefinitions.numberLineWithFractionGroups,
+		numberSetDiagram: allWidgetDefinitions.numberSetDiagram,
+		parallelogramTrapezoidDiagram: allWidgetDefinitions.parallelogramTrapezoidDiagram,
+		partitionedShape: allWidgetDefinitions.partitionedShape,
+		pentagonIntersectionDiagram: allWidgetDefinitions.pentagonIntersectionDiagram,
+		pictograph: allWidgetDefinitions.pictograph,
+		integerChipModel: allWidgetDefinitions.integerChipModel,
+		integerChipLegend: allWidgetDefinitions.integerChipLegend,
+		pointPlotGraph: allWidgetDefinitions.pointPlotGraph,
+		polygonGraph: allWidgetDefinitions.polygonGraph,
+		polyhedronDiagram: allWidgetDefinitions.polyhedronDiagram,
+		polyhedronNetDiagram: allWidgetDefinitions.polyhedronNetDiagram,
+		probabilitySpinner: allWidgetDefinitions.probabilitySpinner,
+		pythagoreanProofDiagram: allWidgetDefinitions.pythagoreanProofDiagram,
+		ratioBoxDiagram: allWidgetDefinitions.ratioBoxDiagram,
+		rectangularFrameDiagram: allWidgetDefinitions.rectangularFrameDiagram,
+		scaleCopiesSlider: allWidgetDefinitions.scaleCopiesSlider,
+		scatterPlot: allWidgetDefinitions.scatterPlot,
+		shapeTransformationGraph: allWidgetDefinitions.shapeTransformationGraph,
+		stackedItemsDiagram: allWidgetDefinitions.stackedItemsDiagram,
+		tapeDiagram: allWidgetDefinitions.tapeDiagram,
+		transformationDiagram: allWidgetDefinitions.transformationDiagram,
+		treeDiagram: allWidgetDefinitions.treeDiagram,
+		triangleDiagram: allWidgetDefinitions.triangleDiagram,
+		unitBlockDiagram: allWidgetDefinitions.unitBlockDiagram,
+		vennDiagram: allWidgetDefinitions.vennDiagram,
+		verticalArithmeticSetup: allWidgetDefinitions.verticalArithmeticSetup,
+		nPolygon: allWidgetDefinitions.nPolygon
 	},
 	widgetTypeKeys: [
 		"threeDIntersectionDiagram",
@@ -119,7 +177,6 @@ export const mathCoreCollection = {
 		"boxPlot",
 		"circleDiagram",
 		"compositeShapeDiagram",
-		"nestedShapeDiagram",
 		"coordinatePlane",
 		"discreteObjectRatioDiagram",
 		"dotPlot",
