1e40cfc971eea9b29ca4f64b171da5219fa93902
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-10-16 12:47:23 -0500

    bloo

--- src/widgets/collections/index.ts
+++ src/widgets/collections/index.ts
@@ -3,19 +3,22 @@ import { fourthGradeMathCollection } from "./fourth-grade-math"
 import { scienceCollection } from "./science"
 import { simpleVisualCollection } from "./simple-visual"
 import { teksMath4Collection } from "./teks-math-4"
+import { mathCoreCollection } from "./math-core"
 
 export const widgetCollections: {
 	readonly all: typeof allWidgetsCollection
 	readonly science: typeof scienceCollection
 	readonly "simple-visual": typeof simpleVisualCollection
 	readonly "fourth-grade-math": typeof fourthGradeMathCollection
-	readonly "teks-math-4": typeof teksMath4Collection
+    readonly "teks-math-4": typeof teksMath4Collection
+    readonly "math-core": typeof mathCoreCollection
 } = {
 	all: allWidgetsCollection,
 	science: scienceCollection,
 	"simple-visual": simpleVisualCollection,
 	"fourth-grade-math": fourthGradeMathCollection,
-	"teks-math-4": teksMath4Collection
+    "teks-math-4": teksMath4Collection,
+    "math-core": mathCoreCollection
 }
 
 export type { WidgetCollection } from "./types"
