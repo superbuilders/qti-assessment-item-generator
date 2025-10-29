f914fe87801a19ce2701919c2acc883b120e44fa
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-10-29 17:39:11 -0500

    bloo

--- packages/app/src/inngest/functions/index.ts
+++ packages/app/src/inngest/functions/index.ts
@@ -1,3 +1,9 @@
+import { authoredAssessmentItemUpsertRequestedFunction } from "./authored-assessment-item-upsert"
+import { templateGenerationRequestedFunction } from "./template-generation"
 import { helloWorldFunction } from "./hello-world"
 
-export const functions = [helloWorldFunction]
+export const functions = [
+	authoredAssessmentItemUpsertRequestedFunction,
+	templateGenerationRequestedFunction,
+	helloWorldFunction
+]
