93d61897216a0b51b4f78e7769e44bfcc9309d1d
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-10-28 22:08:07 -0500

    bloo

--- packages/app/src/env.ts
+++ packages/app/src/env.ts
@@ -1,13 +1,15 @@
-import "dotenv/config"
 import { createEnv } from "@t3-oss/env-core"
+import * as dotenv from "dotenv"
 import { z } from "zod"
+
+dotenv.config({ path: ".env.local" })
 
 export const env = createEnv({
 	server: {
 		AWS_RDS_RESOURCE_ARN: z.string().min(1),
 		AWS_RDS_SECRET_ARN: z.string().min(1),
 		OPENAI_API_KEY: z.string().min(1),
-		INNGEST_EVENT_KEY: z.string().min(1),
+		INNGEST_EVENT_KEY: z.string().min(1).optional(),
 		INNGEST_SIGNING_KEY: z.string().min(1).optional()
 	},
 	runtimeEnv: process.env,
